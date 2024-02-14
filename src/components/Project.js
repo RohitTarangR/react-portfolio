dff1cf69dfa46f8a2e5d3f5e4d4cf5cd29a3f92e
Rohit Tarang R <rohittarang512005@gmail.com>
2024-02-14 15:39:36 +0530

    changes

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,9 @@ const Project = ({item}) => {
     <>
     <div key={item.id} className='flex flex-col items-center text-center'>
         <div className='mb-8'>
-            <img className='rounded-2xl h-60 object-cover border border-gray-600' src={item.image} alt="" />
+          <a href={item.liveUrl}>
+            <img className='rounded-2xl h-60 object-cover border border-gray-600 hover:brightness-75 transition-all duration-300' src={item.image} alt="" />
+          </a>
             <p className='capitalize text-accent text-sm my-3 '>{item.category}</p>
             <h3 className='text-2xl font-semibold capitalize mb-3'>
                 {item.name}
